447be8b404eab5c6a1995f83b588bce273fc2cb4
kiuty <kiuty@192.168.123.106>
2021-07-19 18:54:58 +0900

    0719 데일리과제  부울경_4반_김윤지

---
+++ hwjava_부울경_4반_김윤지/0719/hwjava_01_부울경_4반_김윤지/CircleArea.java
@@ -0,0 +1,11 @@
+package com.java.first;
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		// 반지름이 5cm인 원의 넓이를 구하는 프로그램을 작성하세요.
+		double radius =5;
+		System.out.println("반지름이 5cm인 원의 넓이는" +(radius*radius*3.14)+"cm2입니다.");
+	}
+
+}

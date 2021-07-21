74251088add66516289c94332c252121e49aec8e
kiuty <kiuty@192.168.123.106>
2021-07-22 00:06:51 +0900

    0721 데일리 과제 - 출력 결과가 다르게 나옵니다. 수정해보겠습니다 ㅜㅜ..

---
+++ hwjava_부울경_4반_김윤지/0721/Solution22.java
@@ -0,0 +1,75 @@
+package com.ssafy.algo;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/*  !!!!!~디버깅해서 오류 찾기 ~!!!!!
+ *  소금쟁이가 처음 뛸 때는 3칸 두번 째 2칸 세번쨰 1칸
+ *  연못 밖으로 나가거나 머물고 있는 소금쟁이와 충돌하면 죽는다.
+ *  N은 5~20
+ *  
+ *  출력 결과
+ *  #1 3
+ *  #2 4
+ *  #3 1
+ */
+
+public class Solution22 {
+	public static void main(String[] args) throws FileNotFoundException{
+		System.setIn(new FileInputStream("res/Solution22.txt"));
+		Scanner sc = new Scanner(System.in);
+		//			
+		int[] dr = {-1,1}; //상 하
+		int[] dc = {-1,1}; //좌 우
+		int T = sc.nextInt();	//테스트 케이스
+
+		
+		for(int test_case=1; test_case<=T; test_case++) { 
+			int N = sc.nextInt(); //연못 크기
+			int W = sc.nextInt(); //소금쟁이 수
+			int[][] arr = new int[W][3]; //소금쟁이
+			int[][] map = new int[N][N]; //연못
+			int AnswerW =W;
+			
+			for (int i = 0; i < W; i++) {  //배열에 소금쟁이 데이터 입력받기
+				for (int j = 0; j < 3; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			
+
+			for (int i = 0; i < W; i++) {
+				int nr = arr[i][0];  //시작위치에 소금쟁이
+				int nc = arr[i][1];
+				int dir = arr[i][2];
+				
+				if(map[nr][nc]==1) { //시작 위치가 다른 소금쟁이 있다면 죽음
+					AnswerW--;
+					break;
+				}
+				for (int j = 3; j >0; j--) { //3칸->1칸 점프
+					if(dir ==1) 	 { nr += j*dr[0]; }
+					else if(dir ==2) { nr += j*dr[1]; }
+					else if(dir ==3) { nc += j*dc[0]; }
+					else if(dir ==4) { nc += j*dc[1]; }
+					
+					if(nr< 0 || nr >= N || nc < 0 || nc >= N) { //연못 밖이라면 죽음
+						AnswerW--;
+						break;
+					} 
+					
+					if(map[nr][nc] == 1) { //위치에 다른 소금쟁이 있다면 죽음
+						AnswerW--;
+						break;
+					}
+					map[nr][nc]=1; //소금쟁이 머무는 표시
+ 				}
+			}
+			
+			System.out.println("#"+test_case+" "+AnswerW);
+		}
+		sc.close();
+	}
+
+}

6096742a6283fefe00c91aa41f16b9c9b77669ca
kiuty <kiuty@192.168.123.105>
2021-08-18 01:19:43 +0900

    0817 데일리과제

---
+++ 0817/Main_문제은행_1828_김윤지.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main_문제은행_1828_김윤지 {
+	static class Chemical implements Comparable<Chemical> {
+		int lowTemp;
+		int highTemp;
+		
+		public Chemical(int lowTemp, int highTemp) {
+			this.lowTemp = lowTemp;
+			this.highTemp = highTemp;
+		}
+		
+		@Override
+		public int compareTo(Chemical o) {
+			return this.highTemp -o.highTemp;
+		}
+		
+	}
+	public static void main(String[] args) throws Exception{
+		//Ci는 xi이상 yi이하의 온도에서 보관되어야만 안전하다
+		//모두 보관하기 위해 필요한 적은 수의 냉장고
+		//화학물질의 수 N개 1이상100이하 -> 다음주: 최저보관온도 & 최고보관온도 -270~10000
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); //화학물질 개수
+		
+		ArrayList<Chemical> list = new ArrayList<>(N);
+		String line;
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			list.add(new Chemical(Integer.parseInt(st.nextToken()),
+					Integer.parseInt(st.nextToken())
+					));
+		}
+		
+		Collections.sort(list);
+		
+		//화학 물질의 최저 온도가 이전 화학물질의 최고 온도보다 낮으면 저장가능
+		//=>이전 물질의 최고 온도보다 현재 물질의 최저온도가 높으면 냉장고 추가
+		//현재 최고온도를 최고온도로 설정
+		int high = list.get(0).highTemp;
+		int cnt =1;
+		for (int i = 1; i < N; i++) {
+			if(list.get(i).lowTemp <= high) {
+				if(list.get(i).highTemp < high) {
+					high = list.get(i).highTemp;
+				}
+				continue;
+			}else {
+				high = list.get(i).lowTemp;
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+
+}
---
+++ 0817/Main_문제은행_2247_김윤지.java
@@ -0,0 +1,67 @@
+package jungol;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class jungol_2247 {
+	static class Room implements Comparable<Room> {
+		int stime;
+		int etime;
+		
+		public Room(int stime, int etime) {
+			this.stime = stime;
+			this.etime = etime;
+		}
+		
+		@Override
+		public int compareTo(Room o) {
+			return stime - o.stime;
+		}	
+	}
+
+	public static void main(String[] args) throws Exception {
+		//학생들의 도서관 이용시간을 분석
+		//1. 적어도 한 명 이상이 머물었던 가장 긴 시간
+		//2. 다녀간 전체 시간 중 학생이 한 명도 머물지 않았던 가장 긴 시간
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine()); //정수 N 
+		ArrayList<Room> list = new ArrayList<>(N);
+		String line;
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) { //N개의 줄에 걸쳐 두 정수 S,E가 입력 된다.
+			st = new StringTokenizer(in.readLine());
+			list.add(new Room(Integer.parseInt(st.nextToken()),
+					Integer.parseInt(st.nextToken())
+					));
+		}
+		//리스트 정렬
+		Collections.sort(list);
+		
+		int stayTime = 0; 
+		int emptyTime = 0;
+		
+		int start = list.get(0).stime;
+		int end =list.get(0).etime;
+		for (int i = 1; i < N; i++) {
+			if(end >= list.get(i).stime) { //연속
+				if(end <= list.get(i).etime) {
+					//종료 시간이 바뀌었으므로 갱신
+					end = list.get(i).etime;
+					int Time = list.get(i).etime - list.get(i).stime;	
+					stayTime = Math.max(stayTime, Time);
+				}
+			}else {
+				int empty = list.get(i).stime - end;
+				emptyTime = Math.max(emptyTime, empty);
+				start = list.get(i).stime;
+				end = list.get(i).etime;
+			}
+
+		}
+		System.out.println(stayTime+" "+emptyTime);
+	}
+
+}
